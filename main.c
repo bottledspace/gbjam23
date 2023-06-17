7c881f2be1f23f93c0e3e0b2524b1844aad173c7
bottledspace <113856426+bottledspace@users.noreply.github.com>
2023-06-16 21:41:18 -0400

    Simple background up and working.

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
 #include <gb/gb.h>
+#include <gb/cgb.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "bgtiles.h"
 
+unsigned char plane0[20*18];
 
 void main(void)
 {
-    // Loop forever
-    while(1) {
+    DISPLAY_ON;
+    SHOW_BKG;
 
-
-		// Game main loop processing goes here
-
-
-		// Done processing, yield CPU and wait for start of next frame
-        wait_vbl_done();
+    memset(plane0, 2, 20*18);
+    unsigned r = 1;
+    for (int i = 0; i < 20*18; i++) {
+        r = (75*r+74) % (1UL<<16 + 1);
+        plane0[i] = r%5;
     }
+
+    /*set_bkg_data(0,178,LaroldsJubilantJunkyard_data);
+    set_bkg_tiles(0,0,20,18,LaroldsJubilantJunkyard_map_plane0);
+    set_bkg_palette(0,1,LaroldsJubilantJunkyard_pallette);
+    VBK_REG=1;
+    set_bkg_tiles(0,0,20,18,LaroldsJubilantJunkyard_map_plane1);
+    VBK_REG=0;*/
+    
+    set_bkg_data(0,bgtiles_tile_count,bgtiles_tile_data);
+    set_bkg_tiles(0,0,20,18,plane0);
+
+
 }
